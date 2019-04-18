66bfc3a8d8bbf4e8832ecb2cffe05e9f253d48a2
Johanna Flores Matus <Johanna Flores Matus>
2019-04-18 18:34:42 -0400

    migraciones principales listas

---
+++ database/migrations/2019_04_18_195705_create_clientes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->integer('codigo')->unique(); 
+            $table->integer('rut')->unique();
+            $table->char('dv',1);           
+            $table->string('razon_social');
+            $table->string('direccion');
+            $table->unsignedBigInteger('localidad_id');
+            $table->foreign('localidad_id')->references('id')->on('localidades');
+            $table->unsignedBigInteger('comuna_id');
+            $table->foreign('comuna_id')->references('id')->on('comunas');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}

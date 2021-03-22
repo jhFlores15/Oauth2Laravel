39f583dcba4ca925f94eea3332090581e8c14808
johanna flores <johflores1510@gmail.com>
2021-03-21 21:10:58 -0300

    Update PrimerAdminSeeder.php

--- database/seeds/PrimerAdminSeeder.php
+++ database/seeds/PrimerAdminSeeder.php
@@ -11,19 +11,8 @@ class PrimerAdminSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-     
-        DB::table('users')->insert([
-        	'rut' => '18887056',
-        	'dv' => '2',
-        	'razon_social' => 'Johanna Flores',
-        	'email' => 'johflores1510@gmail.com',
-        	'password' => bcrypt('johflores'),
-        	'password_visible' => 'johflores',
-        	'rol_id' => 1,
-        	'created_at' => Carbon::now(),
-        	'updated_at' => Carbon::now(),
-        ]);
+    {     
+       
          DB::table('users')->insert([
             'rut' => '15283955',
             'dv' => '3',
